15651ab5d25dca8d84cc32fe53590df56da82b50
Arnav Gupta <championswimmer@gmail.com>
2016-01-14 01:28:18 +0530

    fix mailer
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+require("/sendgrid-php/sendgrid-php.php");
 // Check for empty fields
 if(empty($_GET['name'])  		||
    empty($_GET['email']) 		||
@@ -22,6 +23,18 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@tosc.in\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return $callback+'('+true+')';
+// mail($to,$email_subject,$email_body,$headers);
+$sendgrid = new SendGrid('SENDGRID_API_KEY');
+$email = new SendGrid\Email();
+$email
+    ->addTo($to)
+    ->setFrom($email_address)
+    ->setSubject($email_subject)
+    ->setText($email_body)
+;
+
+$sendgrid->send($email);
+
+
+return true;
 ?>
