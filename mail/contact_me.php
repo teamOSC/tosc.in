49192040e31fbac533401698ad478c3990b4b7c3
Arnav Gupta <championswimmer@gmail.com>
2016-01-14 01:13:15 +0530

    fix mailer
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,20 +1,20 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_GET['name'])  		||
+   empty($_GET['email']) 		||
+   empty($_GET['phone']) 		||
+   empty($_GET['message'])	||
+   !filter_var($_GET['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
 	return false;
    }
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$callback = $_POST['callback'];
+$name = $_GET['name'];
+$email_address = $_GET['email'];
+$phone = $_GET['phone'];
+$message = $_GET['message'];
+$callback = $_GET['callback'];
 
 // Create the email and send the message
 $to = 'championswimmer+tosc@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
